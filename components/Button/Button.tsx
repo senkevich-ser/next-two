e62bc8dd3926deba18c8895f16e12cd384430135
Sergey Senkevich <accc75-mail.ru@yandex.ru>
2023-02-20 11:48:01 +0400

    template commit

---
+++ components/Button/Button.tsx
@@ -0,0 +1,35 @@
+import { ButtonProps } from "./Button.props";
+import styles from "./Button.module.css";
+import ArrowIcon from "./arrow.svg";
+import cn from "classnames";
+
+export const Button = ({
+  children,
+  appearance,
+  arrow = "none",
+  className,
+  ...props
+}: ButtonProps): JSX.Element => {
+  return (
+    <>
+      <button
+        className={cn(styles.button, className, {
+          [styles.primary]: appearance == "primary",
+          [styles.ghost]: appearance == "ghost",
+        })}
+        {...props}
+      >
+        {children}
+        {arrow != "none" && (
+          <span
+            className={cn(styles.arrow, className, {
+              [styles.down]: arrow == "down",
+            })}
+          >
+           <ArrowIcon /> 
+          </span>
+        )}
+      </button>
+    </>
+  );
+};

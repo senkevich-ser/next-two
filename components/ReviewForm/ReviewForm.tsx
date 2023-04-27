7631a5c026a2d0ae39350ccce6a6022ffbf2c533
Siarhey <accc75-mail.ru@yandex.ru>
2023-04-27 12:35:39 +0400

    added success inform with styles in ReviewForm element

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -5,6 +5,7 @@ import Input from "../Input/Input";
 import Rating from "../Rating/Rating";
 import Button from "../Button/Button";
 import Textarea from "../Textarea/Textarea";
+import CloseIcon from './close.svg';
 
 
 export default function ReviewForm({
@@ -13,18 +14,25 @@ export default function ReviewForm({
   ...props
 }: ReviewFormProps): JSX.Element {
   return (
+    <>
     <div className={cn(styles.reviewform, className)} {...props}>  
-    <Input/>
-    <Input className={styles.title}/>
+    <Input placeholder="Имя"/>
+    <Input className={styles.title} placeholder="Заголовок отзыва"/>
     <div className={styles.rating}>
       <span>Оценка:</span>
       <Rating rating={2}/>
     </div>
-    <Textarea className={styles.description}/>
+    <Textarea className={styles.description} placeholder="Текст отзыва"/>
     <div className={styles.submit}>
       <Button appearance="primary">Отправить</Button>
       <span className={styles.disclamer}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
     </div>
     </div>
+    <div className={styles.success}>
+    <div className={styles.successtitle}>Ваш отзыв отправлен</div>
+    <div className={styles.successinfo}>Спасибо, Ваш отзыв будет опубликован после проверки.</div>
+      <CloseIcon className={styles.successicon}/>
+    </div>
+    </>
   );
 }
